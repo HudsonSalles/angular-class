f623cf0b193a6a697c01ed6806aec22b7bec73a9
Hudson Salles <hudsonsalless@gmail.com>
2025-12-17 16:28:11 -0300

    feat: add new SVG icons for download, medal, plus, trash, warning, and close actions
    feat: implement item certificate component with styles and template
    
    feat: create navbar component with styles and links
    
    feat: add primary and secondary button components with styles
    
    feat: set up application routing and configuration
    
    chore: initialize application with basic structure and styles
    
    test: add unit tests for app component

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+      console.log('Navbar initialized');
+  }
+
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'gerador-certificado';
+}
