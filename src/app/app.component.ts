e8d5951668c837fdf35f13bebd2c7dd9398f51b3
Hudson Salles <hudsonsalless@gmail.com>
2025-12-18 15:04:56 -0300

    feat: create certificado-form component with HTML, CSS, and integration into app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { CertificadoFormComponent } from './pages/certificado-form/certificado-form.component';
 import { CertificadosComponent } from './pages/certificados/certificados.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, BaseUiComponent, CertificadosComponent],
+  imports: [
+    NavbarComponent,
+    BaseUiComponent,
+    CertificadosComponent,
+    CertificadoFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
