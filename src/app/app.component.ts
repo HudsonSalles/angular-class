faabbbfd460586d6c748dc8edfef41313acc4619
Hudson Salles <hudsonsalless@gmail.com>
2025-12-18 13:23:36 -0300

    feat: create certificados component with HTML, CSS, and button integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
-import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent],
+  imports: [NavbarComponent, BaseUiComponent, CertificadosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gerador-certificado';
