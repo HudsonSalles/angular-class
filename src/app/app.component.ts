e23a4b64d6f64e3dbac5c73b9127fcbaaa6bd555
Hudson Salles <hudsonsalless@gmail.com>
2025-12-18 20:43:34 -0300

    feat: implement certiicado component with HTML structure and CSS styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { CertificadoFormComponent } from './pages/certificado-form/certificado-form.component';
 import { CertificadosComponent } from './pages/certificados/certificados.component';
+import { CertiicadoComponent } from './pages/certiicado/certiicado.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { CertificadosComponent } from './pages/certificados/certificados.compone
     BaseUiComponent,
     CertificadosComponent,
     CertificadoFormComponent,
+    CertiicadoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ src/app/pages/certiicado/certiicado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+
+@Component({
+  selector: 'app-certiicado',
+  imports: [SecondaryButtonComponent],
+  templateUrl: './certiicado.component.html',
+  styleUrl: './certiicado.component.css',
+})
+export class CertiicadoComponent {}
