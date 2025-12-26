67de4ceaeadf9834dac66775d822a1c161495824
Hudson Salles <hudsonsalless@gmail.com>
2025-12-26 17:35:46 -0300

    feat: implement certificado management; bind form inputs to Certificado interface and update activity handling

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
+import { Certificado } from '../../_components/interfaces/certificado';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 
@@ -16,26 +17,43 @@ import { SecondaryButtonComponent } from '../../_components/secondary-button/sec
   styleUrl: './certificado-form.component.css',
 })
 export class CertificadoFormComponent {
-  nome: string = '';
   ativade: string = '';
-  atividades: string[] = [];
+  certificado: Certificado = {
+    nome: '',
+    atividades: [],
+  };
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched;
   }
 
   formValido() {
-    return this.nome.length > 0 && this.atividades.length > 0;
+    return (
+      this.certificado?.nome.length > 0 &&
+      this.certificado?.atividades.length > 0
+    );
   }
 
   adicionarAtividade() {
     if (this.ativade.length > 0) {
-      this.atividades.push(this.ativade);
+      this.certificado.atividades.push(this.ativade);
       this.ativade = '';
     }
   }
 
   removerAtividade(index: number) {
-    this.atividades.splice(index, 1);
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit() {
+    if (!this.formValido()) {
+      return;
+    }
+    this.certificado = {
+      nome: this.certificado.nome,
+      atividades: this.certificado.atividades,
+    };
+
+    console.log('Certificado gerado:', this.certificado);
   }
 }
