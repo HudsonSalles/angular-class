70ae4bcfcd2fd6706e0943a9de7bd7043ab96b72
Hudson Salles <hudsonsalless@gmail.com>
2025-12-27 16:21:21 -0300

    feat: add download functionality for certificado; integrate html2canvas for image generation

--- src/app/pages/certificado/certificado.component.ts
+++ src/app/pages/certificado/certificado.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import html2Canvas from 'html2canvas';
 import { Certificado } from '../../_components/interfaces/certificado';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 import { CertificadoService } from '../../_services/certificado.service';
@@ -14,6 +15,8 @@ export class CertificadoComponent implements OnInit {
   id: string | null = null;
   certificado: Certificado | undefined;
 
+  @ViewChild('certificadoContainer') certificadoElement!: ElementRef;
+
   constructor(
     private certificadoService: CertificadoService,
     private route: ActivatedRoute
@@ -26,5 +29,21 @@ export class CertificadoComponent implements OnInit {
         (item) => item.id === this.id
       );
     });
+  }
+
+  downloadCertificado() {
+    if (this.certificado === undefined) {
+      return;
+    }
+
+    html2Canvas(this.certificadoElement.nativeElement, { scale: 2 }).then(
+      (canvas) => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download =
+          'certificado_' + this.certificado?.nome.replaceAll(' ', '_') + '.png';
+        link.click();
+      }
+    );
   }
 }
